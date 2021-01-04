ea64a2ffe80642c05ce8aa5fd6831cace30814a7
hohyuncheon <75802657+hohyuncheon@users.noreply.github.com>
2021-01-05 01:38:59 +0900

    0104 arraylist

--- baekjoon/src/step3/Test4.java
+++ baekjoon/src/step3/Test4.java
@@ -1,9 +1,20 @@
 package step3;
 
+import java.util.Scanner;
+
 public class Test4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수입력하시오");
+		int num = sc.nextInt();
+		
+		for (int i = 1; i < num; i++) {
+			System.out.println(i);
+			
+		}
 
 	}
 
--- baekjoon/src/step3/Test5.java
+++ baekjoon/src/step3/Test5.java
@@ -1,9 +1,19 @@
 package step3;
 
+import java.util.Scanner;
+
 public class Test5 {
 
 	public static void main(String[] args) {
 
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("정수입력하시오");
+		int num = sc.nextInt();
 
+		for (int i = num; i > 0; i--) {
+			System.out.println(i);
+
+		}
+
+	}
 }
